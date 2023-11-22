419e7081ff7839ee0759f3b4afef62fee1b29e58
icobhavesh <bhaveshsaxena058@gmail.com>
2023-11-22 20:51:40 +0530

    Update in .gitignore  with changes

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <div>
       <BrowserRouter>
-      <Header />
+      {/* <Header /> */}
             <Routes>
               <Route path='/home' element={<Home />} />
               <Route path='/coins' element={<Coins />} />
